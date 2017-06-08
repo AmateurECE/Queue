2386c54c7d0a0c28609d398885278de9e62328cf
AmateurECE <edtwardy@mtu.edu>
2017-06-08 18:21:29 -0500

    Fixed some style issues

--- queuearray.c
+++ queuearray.c
@@ -1,9 +1,16 @@
-/*
- * NAME: queue,c -- a test file for the header in queue.h
- * AUTHOR: Ethan D. Twardy
- * CREATED: 04/14/17
- * LAST EDITED: 04/14/17
- */
+/*******************************************************************************
+ * NAME:	    queuearray.c
+ *
+ * AUTHOR:	    Ethan D. Twardy
+ *
+ * DESCRIPTION:	    Contains the implementation of a Queue, using Arrays, as the
+ *		    name suggests. Also contains test code. Run 'make debug' to
+ *		    compile with the test code.
+ *
+ * CREATED:	    04/14/17
+ *
+ * LAST EDITED:	    06/07/17
+ ***/
 
 /* SAMPLE OUTPUT:
  * ===== Inserting ======
@@ -30,32 +37,180 @@
  * int 6 @ 0x7fbe71d00100
  */
 
+/*******************************************************************************
+ * INCLUDES
+ ***/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#ifdef CONFIG_DEBUG_QARRAY
 #include <assert.h>
 #include <time.h>
+#endif /* CONFIG_DEBUG_QARRAY */
 
 #include "queuearray.h"
 
+/*******************************************************************************
+ * API FUNCTIONS
+ ***/
+
+/*******************************************************************************
+ * FUNCTION:	    queue_init
+ *
+ * DESCRIPTION:	    Initializes a queue pointer to the size specified.
+ *
+ * ARGUMENTS:	    queue: (Queue *) -- the queue to be initialized.
+ *		    destroy: (void (*)(void *)) -- pointer to a user-defined
+ *			     function meant to free data held in the array.
+ *		    size: int -- the size of the queue.
+ *
+ * RETURN:	    void.
+ *
+ * NOTES:	    O(1)
+ ***/
+void queue_init(Queue * queue, void (*destroy)(void *), int size)
+{
+  queue->queue = calloc(size, sizeof(void *));
+  queue->destroy = destroy;
+  queue->front = -1;
+  queue->back = 0;
+  queue->capacity = size;
+  queue->size = 0;
+}
+
+/*******************************************************************************
+ * FUNCTION:	    queue_enqueue
+ *
+ * DESCRIPTION:	    Adds a new element to the stern of the queue.
+ *
+ * ARGUMENTS:	    queue: (Queue *) -- the queue to be operated on.
+ *		    data: (void *) -- pointer to the data to be enqueued.
+ *
+ * RETURN:	    int -- 0 on success, -1 otherwise.
+ *
+ * NOTES:	    O(1)
+ ***/
+int queue_enqueue(Queue * queue, void * data)
+{
+
+  if (queue_isfull(queue))
+    return -1;
+
+  
+  queue->queue[queue->back] = data;
+  queue->back = (queue->back + 1) % queue->capacity;
+
+  if (queue->front == -1)
+    queue->front = queue->back - 1;
+
+  queue->size++;
+  return 0;
+}    
+
+/*******************************************************************************
+ * FUNCTION:	    queue_dequeue
+ *
+ * DESCRIPTION:	    Removes the next element from the queue.
+ *
+ * ARGUMENTS:	    queue: (Queue *) -- the queue to be operated on.
+ *		    data: (void **) -- the data is placed here after dequeueing.
+ *
+ * RETURN:	    int -- 0 on success, -1 otherwise.
+ *
+ * NOTES:	    O(1)
+ ***/
+int queue_dequeue(Queue * queue, void ** data)
+{
+  
+  if (queue_isempty(queue))
+    return -1;
+
+  *data = queue->queue[queue->front];
+  queue->front = (queue->front + 1) % queue->capacity;
+
+  queue->size--;
+
+  if (queue_isempty(queue))
+    queue->front = -1;
+
+  return 0;
+}
+
+/*******************************************************************************
+ * FUNCTION:	    queue_peek
+ *
+ * DESCRIPTION:	    Returns the next element in the queue without removing it.
+ *
+ * ARGUMENTS:	    queue: (Queue *) -- the queue to be operated on.
+ *
+ * RETURN:	    (void *) -- pointer to the next element in the queue, or
+ *		    NULL if the queue is empty.
+ *
+ * NOTES:	    O(1)
+ ***/
+void * queue_peek(Queue * queue)
+{
+  if (queue_isempty(queue))
+    return NULL;
+
+  return queue->queue[queue->front];
+}
+
+/*******************************************************************************
+ * FUNCTION:	    queue_dest
+ *
+ * DESCRIPTION:	    Removes all elements from the queue, and sets all bytes to
+ *		    0. If destroy is set to NULL, does not free the memory held
+ *		    in the queue.
+ *
+ * ARGUMENTS:	    queue: (Queue *) -- the queue to be operated on.
+ *
+ * RETURN:	    void.
+ *
+ * NOTES:	    O(n)
+ ***/
+void queue_dest(Queue * queue)
+{
+  int i = queue->front;
+  while (i < queue->back) {
+    if (queue->queue[i] != NULL && queue->destroy != NULL)
+      queue->destroy(queue->queue[i]);
+    else if (queue->queue[i] != NULL)
+      free(queue->queue[i]);
+
+    i = (i + 1) % queue->capacity;
+  }
+    
+  memset(queue, 0, sizeof(Queue));
+  return;
+}
+
+/*******************************************************************************
+ * MAIN
+ ***/
+
+#ifdef CONFIG_DEBUG_QARRAY
 int main(int argc, char * argv[])
 {
-  int * p; // Use a pointer to the data type for reference.
-  Queue * q; // Pointer to Queue
+  int * p;
+  Queue * q; /* Pointer to Queue */
  
   q = malloc(sizeof(Queue));
-  queue_init(q, NULL, 10); // initialize
+  queue_init(q, NULL, 10); /* initialize */
 
   srand((unsigned)time(NULL));
 
   printf("===== Inserting ======\n");
   for (int i = 0; i < 10; i++) {
-    p = malloc(sizeof(int)); // Allocate space on the heap.
-    *p = rand() % 10; // Assign a value to the space.
+    p = malloc(sizeof(int));
+    *p = rand() % 10;
     printf("int %d @ %p\n", *p, p);
-    queue_enqueue(q, (void *)p); // Queue up the pointer.
+    queue_enqueue(q, (void *)p); /* Queue up the pointer. */
   }
   
-  // Test to ensure that enqueueing on a full queue returns -1.
+  /* Test to ensure that enqueueing on a full queue returns -1. */
   p = malloc(sizeof(int));
   *p = 0;
   assert(queue_enqueue(q, (void *)p) == -1);
@@ -63,18 +218,20 @@ int main(int argc, char * argv[])
 
   printf("====== Removing ======\n");
   while (!queue_isempty(q)) {
-    queue_dequeue(q, (void **)&p); // Dequeue.
+    queue_dequeue(q, (void **)&p);
     printf("int %d @ %p\n", *p, p);
-    free(p); // Free the memory that p points to.
+    free(p);
   }
 
-  // Test to ensure that dequeueing from an empty queue returns -1.
+  /* Test to ensure that dequeueing from an empty queue returns -1. */
   assert(queue_dequeue(q, (void **)&p) == -1);
 
-  // Set all bytes of the queue to 0.
-  // Must call init() in order to be able to use the Queue again.
+  /* Must call init() in order to be able to use the Queue again. */
   queue_dest(q);
   free(q);
 
   return 0;
 }
+#endif /* CONFIG_DEBUG_ARRAY */
+
+/******************************************************************************/
--- queuearray.h
+++ queuearray.h
@@ -1,32 +1,32 @@
-/*
- * TITLE: queue.h -- my custom implementation of a circular queue, using arrays.
- * AUTHOR: Ethan D. Twardy
- * CREATED: 04/05/17
- * LAST EDITED: 04/05/17
- */
+/*******************************************************************************
+ * TITLE:	    queuearray.h
+ *
+ * AUTHOR:	    Ethan D. Twardy
+ *
+ * DESCRIPTION:	    Header file containing macros and typedefs for the C code
+ *		    in queuearray.c.
+ *
+ * CREATED:	    04/05/17
+ *
+ * LAST EDITED:	    06/07/17
+ ***/
 
-/* ************ NOTE ************ */
-/* Due to conflicts between func- */
-/* tion signatures for the public */
-/* interfaces for this structure  */
-/* and the queue using arrays,    */
-/* the two header files cannot    */
-/* be used in the same .c file.   */
-/* This will be patched in a      */
-/* future update.                 */
-/* ****************************** */
+/* ********************************** NOTE ********************************** */
+/* Due to conflicts between function signatures for the public interfaces for */
+/* this structure and the queue using lists, the two header files cannot be  */
+/* used in the same .c file. This will be patched in a future update.         */
+/* ************************************************************************** */
 
-#ifndef __ET_QUEUEARRAY__
-#define __ET_QUEUEARRAY__
+#ifndef __ET_QUEUEARRAY_H__
+#define __ET_QUEUEARRAY_H__
 
-#include <string.h>
-
-/************* TYPE DEFINITIONS *************/
-/********************************************/
+/*******************************************************************************
+ * TYPE DEFINITIONS
+ ***/
 
 typedef struct _Queue_ {
 
-  void (*destroy)(void * data);
+  void (*destroy)(void *);
   int front;
   int back;
   int capacity;
@@ -35,100 +35,24 @@ typedef struct _Queue_ {
 
 } Queue;
 
-/************* MACRO DEFINITIONS ************/
-/********************************************/
+/*******************************************************************************
+ * MACRO DEFINITIONS
+ ***/
 
 #define queue_size(queue) ((queue)->size)
 #define queue_isempty(queue) (queue_size(queue) == 0 ? 1 : 0)
 #define queue_isfull(queue) (queue_size(queue) == (queue)->capacity ? 1 : 0)
 
-/************* PUBLIC INTERFACE ************/
-/*******************************************/
+/*******************************************************************************
+ * API FUNCTION PROTOTYPES
+ ***/
 
-// FUNCTION: init O(1) -- initializes the queue to the size specified in size.
-// PARAMETERS: queue: (Queue *) -- the queue to be initialized.
-//             destroy: void (*)(void *) -- user defined function to free data
-// RETURN: void
-void queue_init(Queue * queue, void (*destroy)(void *), int size)
-{
-  queue->queue = calloc(size, sizeof(void *));
-  queue->destroy = destroy;
-  queue->front = -1;
-  queue->back = 0;
-  queue->capacity = size;
-  queue->size = 0;
-}
+extern void queue_init(Queue *, void (*)(void *), int);
+extern int queue_enqueue(Queue *, void *);
+extern int queue_dequeue(Queue *, void **);
+extern void * queue_peek(Queue *);
+extern void queue_dest(Queue *);
 
-// FUNCTION: enquque O(1) -- adds a new element to the queue
-// PARAMETERS:
-// RETURN: int -- -1 on failure, 0 on success
-int queue_enqueue(Queue * queue, void * data)
-{
+#endif /* __ET_QUEUEARRAY_H__ */
 
-  if (queue_isfull(queue))
-    return -1;
-
-  
-  queue->queue[queue->back] = data;
-  queue->back = (queue->back + 1) % queue->capacity;
-
-  if (queue->front == -1)
-    queue->front = queue->back - 1;
-
-  queue->size++;
-  return 0;
-}    
-
-// FUNCTION: dequeue O(1) -- removes the next element from the queue.
-// PARAMETERS:
-// RETURN: int -- -1 on failure, 0 on success.
-int queue_dequeue(Queue * queue, void ** data)
-{
-  
-  if (queue_isempty(queue))
-    return -1;
-
-  *data = queue->queue[queue->front];
-  queue->front = (queue->front + 1) % queue->capacity;
-
-  queue->size--;
-
-  if (queue_isempty(queue))
-    queue->front = -1;
-
-  return 0;
-}
-
-// FUNCTION: peek O(1) -- returns but does not remove the next element
-//           in the queue.
-// PARAMETERS:
-// RETURN: (void *) -- the next element in the queue.
-void * queue_peek(Queue * queue)
-{
-  if (queue_isempty(queue))
-    return NULL;
-
-  return queue->queue[queue->front];
-}
-
-// FUNCTION: dest O(n) -- removes all elements from the queue and sets
-//           all bytes of memory in the queue to 0.
-// PARAMETERS: queue: (Queue *) -- the queue to be operated on.
-// RETURN: none.
-void queue_dest(Queue * queue)
-{
-  int i = queue->front;
-  while (i < queue->back) {
-    if (queue->queue[i] != NULL && queue->destroy != NULL)
-      queue->destroy(queue->queue[i]);
-    else if (queue->queue[i] != NULL)
-      free(queue->queue[i]);
-
-    i = (i + 1) % queue->capacity;
-  }
-    
-  memset(queue, 0, sizeof(Queue));
-  return;
-}
-
-#endif // __ET_QUEUEARRAY__
+/******************************************************************************/
